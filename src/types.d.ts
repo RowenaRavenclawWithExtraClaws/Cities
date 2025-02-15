957d727df436f793c64cacb266456ab13da2d0f5
RowenaRavenclawWithExtraClaws <abdelrahman.abdelsalam@clark.io>
2025-02-15 09:31:16 +0100

    feat: create the new city-card component

---
+++ src/types.d.ts
@@ -0,0 +1,11 @@
+interface City {
+  name?: string;
+  nameNative?: string;
+  country: string;
+  continent?: string;
+  latitude?: number;
+  longitude?: number;
+  population: number;
+  founded?: string;
+  landmarks?: string[];
+}
