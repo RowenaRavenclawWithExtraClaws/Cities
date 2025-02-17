c926af318e30ecc4d46f470c9d44886cdb7f97ad
Abdelrahman Nasser <41797034+RowenaRavenclawWithExtraClaws@users.noreply.github.com>
2025-02-17 01:21:18 +0100

    Feature branch layout (#4)
    * [feat] create main app layout
    
    * [feat] render the layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,1 +1,21 @@
-export const Layout = () => {};
+import react, { ReactNode } from "react";
+
+type Props = {
+  left: ReactNode;
+  right: ReactNode;
+};
+
+export const Layout = ({left, right}: Props) => {
+  return (
+    <div className="flex h-screen">
+      {/* Left side */}
+      <div className="w-1/4 max-h-screen overflow-y-auto">
+        {left}
+      </div>
+      {/* Right side */}
+      <div className="w-3/4">
+        {right}
+      </div>
+    </div>
+  );
+};
